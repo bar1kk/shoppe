5468e977a73e0d67b10cb27149c78678dd166ab4
danil <danil22344bel@gmail.com>
2023-11-02 23:37:31 +0200

    docs(swagger): added documentation on the login method

--- backend/src/main/java/dev/danilbel/backend/controller/AuthController.java
+++ backend/src/main/java/dev/danilbel/backend/controller/AuthController.java
@@ -3,7 +3,13 @@ package dev.danilbel.backend.controller;
 import dev.danilbel.backend.controller.helper.ControllerHelper;
 import dev.danilbel.backend.dto.auth.JwtRequestDto;
 import dev.danilbel.backend.dto.auth.JwtResponseDto;
+import dev.danilbel.backend.dto.exception.ExceptionResponseDto;
 import dev.danilbel.backend.service.JwtTokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -20,6 +26,7 @@ import jakarta.validation.Valid;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Auth")
 public class AuthController {
 
     private static final String LOGIN = "api/v1/auth/login";
@@ -28,6 +35,25 @@ public class AuthController {
 
     JwtTokenService jwtTokenService;
 
+    @Operation(
+            summary = "Login",
+            description = "Getting a jwt token",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok. User has been successfully issued a token"),
+                    @ApiResponse(responseCode = "400", description = "Bad Request. Validation error JwtRequest",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized. Invalid email or password",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Not Found. User not found",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "405", description = "Method Not Allowed",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    )
+            }
+    )
     @PostMapping(LOGIN)
     public ResponseEntity<JwtResponseDto> login(@RequestBody @Valid JwtRequestDto jwtRequestDto,
                                                 BindingResult bindingResult) {
