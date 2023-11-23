44c91068b46dcfc13c8e4af25d8064dbca5506ca
danil <danil22344bel@gmail.com>
2023-11-23 15:32:44 +0200

    feat(service): added user creation

--- backend/src/main/java/dev/danilbel/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/dev/danilbel/backend/service/impl/UserServiceImpl.java
@@ -1,14 +1,22 @@
 package dev.danilbel.backend.service.impl;
 
+import dev.danilbel.backend.dto.user.RegistrationRequestDto;
+import dev.danilbel.backend.dto.user.UserDto;
+import dev.danilbel.backend.entity.RoleEntity;
 import dev.danilbel.backend.entity.UserEntity;
+import dev.danilbel.backend.exception.AlreadyExistsException;
 import dev.danilbel.backend.exception.NotFoundException;
+import dev.danilbel.backend.mapper.UserMapper;
 import dev.danilbel.backend.repository.UserRepository;
+import dev.danilbel.backend.service.RoleService;
 import dev.danilbel.backend.service.UserService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -17,6 +25,10 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     UserRepository userRepository;
+
+    RoleService roleService;
+
+    UserMapper userMapper;
 
     @Override
     public UserEntity getUserEntityByEmail(String email) {
@@ -32,5 +44,30 @@ public class UserServiceImpl implements UserService {
 
         log.info("IN UserServiceImpl.getUserEntityByEmail - user: {} found by email '{}'", result, email);
         return result;
+    }
+
+    @Override
+    public UserDto createUser(RegistrationRequestDto registrationRequestDto) {
+
+        userRepository.findByEmail(registrationRequestDto.getEmail()).ifPresent(
+                userEntity -> {
+                    log.error("IN UserServiceImpl.createUser - user with email '{}' already exists", registrationRequestDto.getEmail());
+                    throw new AlreadyExistsException(
+                            String.format("User with email '%s' already exists", registrationRequestDto.getEmail())
+                    );
+                }
+        );
+
+        RoleEntity roleEntity = roleService.getRoleEntityByName("ROLE_USER");
+
+        UserEntity userEntity = UserEntity.builder()
+                .email(registrationRequestDto.getEmail())
+                .password(registrationRequestDto.getPassword())
+                .roles(Set.of(roleEntity))
+                .build();
+
+        UserEntity savedUserEntity = userRepository.save(userEntity);
+
+        return userMapper.toDto(savedUserEntity);
     }
 }
