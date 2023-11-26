7145d4fe1cb41f6c5b8dae269db11651dc7475f1
danil <danil22344bel@gmail.com>
2023-11-26 21:13:00 +0200

    docs(swagger): added documentation

--- backend/src/main/java/dev/danilbel/backend/controller/SuperAdminUserController.java
+++ backend/src/main/java/dev/danilbel/backend/controller/SuperAdminUserController.java
@@ -1,7 +1,12 @@
 package dev.danilbel.backend.controller;
 
+import dev.danilbel.backend.dto.exception.ExceptionResponseDto;
 import dev.danilbel.backend.dto.user.UserDto;
 import dev.danilbel.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
@@ -30,6 +35,16 @@ public class SuperAdminUserController {
 
     UserService userService;
 
+    @Operation(
+            summary = "Get all admins",
+            description = "Getting all admins",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok. Admins have been successfully received"),
+                    @ApiResponse(responseCode = "405", description = "Method Not Allowed",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    )
+            }
+    )
     @GetMapping(ADMINS)
     public ResponseEntity<List<UserDto>> getAllAdmins() {
 
@@ -38,6 +53,22 @@ public class SuperAdminUserController {
         return ResponseEntity.ok(result);
     }
 
+    @Operation(
+            summary = "Set admin role for user",
+            description = "Setting admin role for user",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok. Admin role has been successfully set"),
+                    @ApiResponse(responseCode = "404", description = "Not Found. User not found",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "405", description = "Method Not Allowed",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "409", description = "Conflict. User already has admin role",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    )
+            }
+    )
     @PutMapping(SET_ADMIN)
     public ResponseEntity<UserDto> setAdminRole(@PathVariable("id") String id) {
 
@@ -46,6 +77,22 @@ public class SuperAdminUserController {
         return ResponseEntity.ok(result);
     }
 
+    @Operation(
+            summary = "Remove admin role for user",
+            description = "Removing admin role for user",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok. Admin role has been successfully removed"),
+                    @ApiResponse(responseCode = "404", description = "Not Found. User not found",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "405", description = "Method Not Allowed",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "409", description = "Conflict. User already has not admin role",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))
+                    )
+            }
+    )
     @PutMapping(REMOVE_ADMIN)
     public ResponseEntity<UserDto> removeAdminRole(@PathVariable("id") String id) {
 
