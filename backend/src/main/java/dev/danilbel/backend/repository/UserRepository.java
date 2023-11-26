d238ea1c0c3598ef184f228e6014aa07569345dc
danil <danil22344bel@gmail.com>
2023-11-26 12:38:53 +0200

    feat(repository): added search for users by status

--- backend/src/main/java/dev/danilbel/backend/repository/UserRepository.java
+++ backend/src/main/java/dev/danilbel/backend/repository/UserRepository.java
@@ -1,13 +1,19 @@
 package dev.danilbel.backend.repository;
 
 import dev.danilbel.backend.entity.UserEntity;
+import dev.danilbel.backend.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
     Optional<UserEntity> findByEmail(String email);
+
+    Stream<UserEntity> streamAllBy();
+
+    Stream<UserEntity> streamAllByStatus(UserStatus status);
 }
