a6ec15e2c27aae867550bc338dba7262c3a1e084
danil <danil22344bel@gmail.com>
2023-11-26 21:06:06 +0200

    feat(config): added requestMatchers SUPER_ADMIN

--- backend/src/main/java/dev/danilbel/backend/config/SecurityConfig.java
+++ backend/src/main/java/dev/danilbel/backend/config/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html"
                         ).permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/super-admin/**").hasRole("SUPER_ADMIN")
                         .anyRequest().permitAll()
                 )
                 .apply(jwtConfigurer);
