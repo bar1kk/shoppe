013301830b75d62b2c9b8f5b23aea050144d778f
bar1kk <barbar.vladyslav@lll.kpi.ua>
2023-12-05 00:36:34 +0200

    feat(component): add footer

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from '../header/Header';
+import Footer from '../footer/Footer';
 
 const App = () => {
     return (
         <Router>
             <div className='App'>
                 <Header line={true}/>
+                <Footer />
             </div>
         </Router>
     );
