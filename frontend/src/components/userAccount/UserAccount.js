bbf9825ee53d4fd05828acf9e3802134a9e85de2
bar1kk <barbar.vladyslav@lll.kpi.ua>
2023-12-06 23:23:27 +0200

    feat(component): added dashboard

--- frontend/src/components/userAccount/UserAccount.js
+++ frontend/src/components/userAccount/UserAccount.js
@@ -1,9 +1,10 @@
 import { useAuthHeader } from 'react-auth-kit';
 import { useSelector, useDispatch } from 'react-redux';
-
-import './userAccount.scss';
 import { useEffect } from 'react';
 import { addedUserHeader, changeFilter } from './UserAccountSlice';
+
+import './userAccount.scss';
+import Dashboard from './dashboard/Dashboard';
 
 const UserAccount = () => {
     const { filter } = useSelector((state) => state.userAccount);
@@ -22,9 +23,12 @@ const UserAccount = () => {
     return (
         <div className='account'>
             <div className='container'>
-                <div className='account__title'>
-                    <h1>My Account</h1>
-                </div>
+                {filter === 'dashboard' && ( // Використовуємо умовний оператор для відображення заголовка
+                    <div className='account__title'>
+                        <h1>My Account</h1>
+                    </div>
+                )}
+
                 <div className='account__nav'>
                     <ul className='account__nav-wrapper'>
                         <li>
@@ -40,48 +44,50 @@ const UserAccount = () => {
                             </a>
                         </li>
                         <li>
-                            <a href='#!'
-                            onClick={() => onChangeFilter('orders')}
-                            className={
-                                filter === 'orders'
-                                    ? 'account__nav-link account__nav-active'
-                                    : 'account__nav-link'
-                            }
-                            >Orders</a>
+                            <a
+                                href='#!'
+                                onClick={() => onChangeFilter('orders')}
+                                className={
+                                    filter === 'orders' ? 'account__nav-link account__nav-active' : 'account__nav-link'
+                                }>
+                                Orders
+                            </a>
                         </li>
                         <li>
-                            <a href='#!'
-                            onClick={() => onChangeFilter('addresses')}
-                            className={
-                                filter === 'addresses'
-                                    ? 'account__nav-link account__nav-active'
-                                    : 'account__nav-link'
-                            }
-                            >Addresses</a>
+                            <a
+                                href='#!'
+                                onClick={() => onChangeFilter('addresses')}
+                                className={
+                                    filter === 'addresses'
+                                        ? 'account__nav-link account__nav-active'
+                                        : 'account__nav-link'
+                                }>
+                                Addresses
+                            </a>
                         </li>
                         <li>
-                            <a href='#!'
-                            onClick={() => onChangeFilter('details')}
-                            className={
-                                filter === 'details'
-                                    ? 'account__nav-link account__nav-active'
-                                    : 'account__nav-link'
-                            }
-                            >Account details</a>
+                            <a
+                                href='#!'
+                                onClick={() => onChangeFilter('details')}
+                                className={
+                                    filter === 'details' ? 'account__nav-link account__nav-active' : 'account__nav-link'
+                                }>
+                                Account details
+                            </a>
                         </li>
                         <li>
-                            <a href='#!'
-                            onClick={() => onChangeFilter('logout')}
-                            className={
-                                filter === 'logout'
-                                    ? 'account__nav-link account__nav-active'
-                                    : 'account__nav-link'
-                            }
-                            >Logout</a>
+                            <a
+                                href='#!'
+                                onClick={() => onChangeFilter('logout')}
+                                className={
+                                    filter === 'logout' ? 'account__nav-link account__nav-active' : 'account__nav-link'
+                                }>
+                                Logout
+                            </a>
                         </li>
                     </ul>
                     <div className='account__nav-line'></div>
-                    
+                    {filter === 'dashboard' ? <Dashboard onChangeFilter={onChangeFilter}/> : null}
                 </div>
             </div>
         </div>
---
+++ frontend/src/components/userAccount/dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import './dashboard.scss';
+import {useAuthUser} from 'react-auth-kit'
+
+
+const Dashboard = ({ onChangeFilter }) => {
+    const auth = useAuthUser()
+
+    return (
+        <div className='dashboard__wrapper'>
+            <p>
+                Hello {auth()} (not {auth()}? <span onClick={() => onChangeFilter('logout')} className='dashboard__link'>Log out</span>)
+                <br />
+                From your account dashboard you can view your <span onClick={() => onChangeFilter('orders')} className='dashboard__link'>recent orders</span>,
+                manage your <span onClick={() => onChangeFilter('addresses')} className='dashboard__link'>shipping addresses</span>, and edit your{' '}
+                <span onClick={() => onChangeFilter('details')} className='dashboard__link'>password and account details</span>.
+            </p>
+        </div>
+    );
+};
+
+export default Dashboard;
