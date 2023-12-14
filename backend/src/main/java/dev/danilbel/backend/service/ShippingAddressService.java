35a8304520edd9fa3274607084d3aa779448790c
danil <danil22344bel@gmail.com>
2023-12-15 01:59:55 +0200

    feat(service): added ShippingAddressService

---
+++ backend/src/main/java/dev/danilbel/backend/service/ShippingAddressService.java
@@ -0,0 +1,12 @@
+package dev.danilbel.backend.service;
+
+import dev.danilbel.backend.dto.address.ShippingAddressDto;
+
+public interface ShippingAddressService {
+
+    ShippingAddressDto getShippingAddressById(String id);
+
+    ShippingAddressDto createShippingAddress(String userEmail, ShippingAddressDto shippingAddressDto);
+
+    void removeShippingAddress(String id);
+}
