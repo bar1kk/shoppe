6d2a00412acd6258642cb68b57abc1a47bbf9db1
danil <danil22344bel@gmail.com>
2023-12-18 23:48:53 +0200

    feat(repository): added a method to get a stream of all the user's shipping addresses

--- backend/src/main/java/dev/danilbel/backend/repository/ShippingAddressRepository.java
+++ backend/src/main/java/dev/danilbel/backend/repository/ShippingAddressRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Repository
 public interface ShippingAddressRepository extends JpaRepository<ShippingAddressEntity, String> {
 
     Optional<ShippingAddressEntity> findByIdAndStatus(String id, ShippingAddressStatus status);
+
+    Stream<ShippingAddressEntity> streamAllByUserIdAndStatus(String userId, ShippingAddressStatus status);
 }
