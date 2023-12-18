3eb0dc4e5f5c595aecf7f749e08491a9c01eeda1
danil <danil22344bel@gmail.com>
2023-12-18 23:31:34 +0200

    feat(repository): added method to find with status

--- backend/src/main/java/dev/danilbel/backend/repository/ShippingAddressRepository.java
+++ backend/src/main/java/dev/danilbel/backend/repository/ShippingAddressRepository.java
@@ -1,9 +1,14 @@
 package dev.danilbel.backend.repository;
 
 import dev.danilbel.backend.entity.ShippingAddressEntity;
+import dev.danilbel.backend.enums.ShippingAddressStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShippingAddressRepository extends JpaRepository<ShippingAddressEntity, String> {
+
+    Optional<ShippingAddressEntity> findByIdAndStatus(String id, ShippingAddressStatus status);
 }
