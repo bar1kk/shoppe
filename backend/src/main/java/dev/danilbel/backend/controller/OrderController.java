872cdb55781eea4b36eb43774a0e8199cdcdc2c2
danil <danil22344bel@gmail.com>
2023-12-19 06:10:46 +0200

    feat(service&controller): added possibility to create an order

--- backend/src/main/java/dev/danilbel/backend/controller/OrderController.java
+++ backend/src/main/java/dev/danilbel/backend/controller/OrderController.java
@@ -1,6 +1,8 @@
 package dev.danilbel.backend.controller;
 
+import dev.danilbel.backend.controller.helper.ControllerHelper;
 import dev.danilbel.backend.dto.order.OrderDto;
+import dev.danilbel.backend.dto.order.OrderRequestDto;
 import dev.danilbel.backend.service.OrderService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,8 +11,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 import java.security.Principal;
 import java.util.List;
@@ -27,10 +34,24 @@ public class OrderController {
 
     OrderService orderService;
 
+    ControllerHelper controllerHelper;
+
     @GetMapping(ORDERS)
     public ResponseEntity<List<OrderDto>> getAllOrders(Principal principal) {
 
         List<OrderDto> result = orderService.getAllOrders(principal.getName());
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping(ORDERS)
+    public ResponseEntity<OrderDto> createOrder(@RequestBody @Valid OrderRequestDto orderRequestDto,
+                                                Principal principal,
+                                                BindingResult bindingResult) {
+
+        controllerHelper.checkBindingResultElseThrowException(bindingResult, "OrderController.createOrderByUserEmail");
+
+        OrderDto result = orderService.createOrderByUserEmail(principal.getName(), orderRequestDto);
 
         return ResponseEntity.ok(result);
     }
