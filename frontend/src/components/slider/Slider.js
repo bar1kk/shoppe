8e2519351f6e8b08fe2fb1a341d4987e62a272b9
bar1kk <barbar.vladyslav@lll.kpi.ua>
2023-12-19 03:31:22 +0200

    feat(component): updated slider component

--- frontend/src/components/slider/Slider.js
+++ frontend/src/components/slider/Slider.js
@@ -6,25 +6,42 @@ import 'swiper/css/pagination';
 
 import SliderItem from './SliderItem';
 import './slider.scss';
+import sliderImage1 from '../../assets/images/slider/slider-1.webp';
+import sliderImage2 from '../../assets/images/slider/slider-2.webp';
+import sliderImage3 from '../../assets/images/slider/slider-3.webp';
+import sliderImage4 from '../../assets/images/slider/slider-4.webp';
 
 const Slider = () => {
-    return(
-        <div className="slider">
-            <div className="container">
-                <div className="slider__wrapper">
-                <Swiper 
-                    pagination={true} 
-                    modules={[Pagination]} 
-                    className="mySwiper">
-                    <SwiperSlide><SliderItem/></SwiperSlide>
-                    <SwiperSlide><SliderItem/></SwiperSlide>
-                    <SwiperSlide><SliderItem/></SwiperSlide>
-                    <SwiperSlide><SliderItem/></SwiperSlide>
-                </Swiper>
+    return (
+        <div className='slider'>
+            <div className='container'>
+                <div className='slider__wrapper'>
+                    <Swiper pagination={true} modules={[Pagination]} className='mySwiper'>
+                        <SwiperSlide>
+                            <SliderItem image={sliderImage1} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <SliderItem image={sliderImage2} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <SliderItem image={sliderImage3} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <SliderItem image={sliderImage4} />
+                        </SwiperSlide>
+                    </Swiper>
+                    <div className='slider-text'>
+                        Iconic & fine jewelry, <br />
+                        for every woman.
+                    </div>
+                    <div className='slider-paragraph'>
+                        Every woman deserves to feel confident. Discover our unique jewelry, crafted with <br /> premium
+                        stainless steel and 18K gold plated.
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Slider;
