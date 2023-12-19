f19998b6f3a943e81f4c7da8304c002ab9aa1b26
danil <danil22344bel@gmail.com>
2023-12-19 06:05:15 +0200

    feat(dto): added a dtos to create an order

---
+++ backend/src/main/java/dev/danilbel/backend/dto/order/OrderItemRequestDto.java
@@ -0,0 +1,30 @@
+package dev.danilbel.backend.dto.order;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class OrderItemRequestDto {
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity should be greater than 0")
+    Integer quantity;
+
+    @JsonProperty("product_id")
+    @NotBlank(message = "Product id cannot be blank")
+    @Size(min = 36, max = 36, message = "Product id should be 36 characters long")
+    String productId;
+}
