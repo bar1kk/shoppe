40b548367bb94a0c475f02c709d3d5fb2a712f45
bar1kk <barbar.vladyslav@lll.kpi.ua>
2023-12-20 15:45:51 +0200

    feat(component): updated jewelry catalog promo according to the new API

--- frontend/src/components/jewelryCatalog/JewelryCatalogPromo.js
+++ frontend/src/components/jewelryCatalog/JewelryCatalogPromo.js
@@ -1,38 +1,57 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPromoGoods } from './JewelryCatalogSlice';
+import { fetchPromoGoods, fetchGoods, fetchPromoGoodsId } from './JewelryCatalogSlice';
 
 import './jewelryCatalog.scss';
 import JewelryItemPromo from '../jewelryItem/JewelryItemPromo';
 import Spinner from '../spinner/Spinner';
 
 const JewelryCatalogPromo = () => {
-    const { promoGoods, goodsLoadingStatus } = useSelector((state) => state.goods);
+    const { promoGoods, promoGoodsId, goods, goodsLoadingStatus, promoGoodsLoadingStatus } = useSelector(
+        (state) => state.goods
+    );
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(fetchPromoGoods());
+        dispatch(fetchGoods());
+        dispatch(fetchPromoGoodsId());
         //eslint-disable-next-line
     }, []);
 
-    if (goodsLoadingStatus === 'loading') {
+    useEffect(() => {
+        if (promoGoodsId.length > 0 && goods.length > 0) {
+            const promoGoods = promoGoodsId.map(({ id }) => {
+                return goods.find((item) => item.id === id);
+            });
+            dispatch(fetchPromoGoods(promoGoods));
+        }
+        //eslint-disable-next-line
+    }, [promoGoodsId, goods]);
+
+    const renderCatalog = (items) => {
+        if (items.length === 0) {
+            const copyOfGoods = [...goods];
+            const shuffledGoods = copyOfGoods.sort(() => Math.random() - 0.5);
+            items = shuffledGoods.slice(0, 3);
+        }
+
+        const itemsList = items.map(({ id, name, price, images }) => {
+            return <JewelryItemPromo name={name} price={price} imagePath={images[0]} key={id} id={id} />;
+        });
+
+        return itemsList;
+    };
+
+    if (goodsLoadingStatus === 'loading' || promoGoodsLoadingStatus === 'loading') {
         return <Spinner />;
-    } else if (goodsLoadingStatus === 'error') {
+    } else if (goodsLoadingStatus === 'error' || promoGoodsLoadingStatus === 'error') {
         return <h5 className='no-results-message'>An error occurred while loading the data</h5>;
     }
-
-    const renderCatalog = (goods) => {
-        const goodsList = goods.map((item) => {
-            return <JewelryItemPromo name={item.name} price={item.price} imagePath={item.imagePath.main} key={item.id} id={item.id} />;
-        });
-        return goodsList;
-    };
 
     const promGoodsList = renderCatalog(promoGoods);
 
     return (
         <div className='catalog'>
-            
             <div className='catalog__wrapper'>{promGoodsList}</div>
         </div>
     );
