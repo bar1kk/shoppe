8b7cf6521ce1679de7e7f8b5890f62755e3f2478
danil <danil22344bel@gmail.com>
2023-12-21 21:46:43 +0200

    feat(migration): added a trigger to refresh the view

--- backend/src/main/resources/db/migration/V14__add_best_selling_products_view.sql
+++ backend/src/main/resources/db/migration/V14__add_best_selling_products_view.sql
@@ -1,4 +1,4 @@
-CREATE VIEW best_selling_products_view AS
+CREATE MATERIALIZED VIEW best_selling_products_view AS
 SELECT p.id             AS id,
        p.name           AS name,
        SUM(oi.quantity) AS total_purchases
@@ -8,3 +8,18 @@ FROM products p
 GROUP BY p.id
 ORDER BY total_purchases DESC
 LIMIT 6;
+
+CREATE OR REPLACE FUNCTION refresh_best_selling_products_view()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    REFRESH MATERIALIZED VIEW best_selling_products_view;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER refresh_best_selling_products_trigger
+    AFTER INSERT
+    ON order_items
+    FOR EACH ROW
+EXECUTE FUNCTION refresh_best_selling_products_view();
