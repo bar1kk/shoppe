7e11776f4326a733668eb48ed7a8b48f62adfe88
danil <danil22344bel@gmail.com>
2023-12-21 02:54:23 +0200

    feat(migration): insert products

---
+++ backend/src/main/resources/db/migration/V16.1__insert_products.sql
@@ -0,0 +1,17 @@
+INSERT INTO colors (name)
+VALUES ('White'),
+       ('Red'),
+       ('Yellow'),
+       ('Black');
+
+INSERT INTO materials (name)
+VALUES ('Gold'),
+       ('Silver');
+
+INSERT INTO types (name)
+VALUES ('Rings'),
+       ('Earrings'),
+       ('Chains'),
+       ('Bracelets'),
+       ('Religion and symbols'),
+       ('Pins');
